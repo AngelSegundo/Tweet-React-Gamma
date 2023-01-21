62bc2cd98f24b0ba9cfeb9572efbf05b6824f9cc
Angel Rodriguez <angel.segundo.dev@gmail.com>
2023-01-21 10:19:17 +0100

    Upgrade Actioins Component

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,11 +1,8 @@
+import Actions from "./Actions";
 import Message from "./Message";
 import ProfileImage from "./ProfileImage";
 import Timestamp from "./Timestamp";
 import User from "./User";
-
-
-
-
 
 function Tweet(props) {
   return (
@@ -19,17 +16,10 @@ function Tweet(props) {
           <Timestamp time={props.tweet.timestamp} />
         </div>
 
-        <p className="message">
-          <Message message={props.tweet.message} />
-        </p>
+        <Message message={props.tweet.message} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Actions />
+
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
